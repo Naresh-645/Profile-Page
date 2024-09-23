47d2db49daebe32c812e042ba969c28f73a89d3f
Naresh-645 <104761375+Naresh-645@users.noreply.github.com>
2024-09-23 14:59:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ document.addEventListener('click', function(event) {
     }
   });
 
+document.querySelectorAll('.nav-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
